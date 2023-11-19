3886e9343e4cf1a801694139066ca6836cf322bc
04megumi <144149012+04megumi@users.noreply.github.com>
2023-11-19 21:58:48 +0800

    Add files via upload

--- 01_running_letter.cpp
+++ 01_running_letter.cpp
@@ -3,36 +3,38 @@
 #include<unistd.h>
 using namespace std;
 
-void movement ( char letter,float speed)
+void movement(char letter, float speed);
+
+int main()
 {
-    int length = 30;
-    int position = 0;
-    int direction = 1;
-
-    while (1)
-    {
-        if(direction==1 && position>=length) direction = -1;
-        if(direction==-1 && position<=0) direction = 1;
-        for(int i=1;i<=length;i++)
-        {
-            system("cls");
-            for (int i = 0; i<=position; i++)
-            {
-                cout << " ";
-            }
-            cout<<letter;
-            position += direction;
-            usleep(speed * 1000000);
-        }
-    }
-}
-
-int main(){
-    movement('@',0.1);
+    movement('6',0.1);
     return 0;
 }
 
+void movement(char letter, float speed)
+{
+int length = 30;
+int position = 0;
+int direction = 1;
 
+while (true)
+{
+if(direction==1 && position>=length) direction = -1;
+if(direction==-1 && position<=0) direction = 1;
+
+for(int i=1;i<=length;i++)
+{
+system("cls");
+    for (int i = 0; i<=position; i++)
+    {
+    cout << " ";
+    }
+    cout<<letter;
+    position += direction;
+    usleep(speed * 1000000);
+    }
+    }
+}
 
 
 
